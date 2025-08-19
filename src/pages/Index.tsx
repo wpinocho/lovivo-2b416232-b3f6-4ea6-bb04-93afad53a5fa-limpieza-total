aab2293a5930cb521b3010eb6d5771a3fbb964d9
Lovivo Restore <restore@lovivo.ai>
2025-08-19 12:50:51 -0600

    Restore to message 7010d29e-f737-4e3d-8d62-cf78cdee6363
    Restored from SHA: 6582a7bc31e3f9f016a99da976d44ad55b11146f

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
-import Header from '../components/Header';
+import React from 'react';
 import Store from '../components/Store';
-import Cart from '../components/Cart';
-import { CartProvider } from '../contexts/CartContext';
+import Layout from '../components/Layout';
 
 const Index = () => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-
   return (
-    <CartProvider>
-      <div className="min-h-screen bg-gray-50">
-        <Header onCartClick={() => setIsCartOpen(true)} />
-        <Store />
-        <Cart isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
-      </div>
-    </CartProvider>
+    <Layout>
+      <Store />
+    </Layout>
   );
 };
 
