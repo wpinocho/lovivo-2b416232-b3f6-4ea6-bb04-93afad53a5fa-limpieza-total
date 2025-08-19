2d4afa570168ff8a64c56507b83e7df6c876a5ee
wpinocho <julian.ruiz.loza@gmail.com>
2025-08-19 12:04:52 -0600

    AI Update: 3 files updated by Lovivo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 import Store from '../components/Store';
 import Cart from '../components/Cart';
 import { CartProvider } from '../contexts/CartContext';
@@ -11,6 +12,7 @@ const Index = () => {
     <CartProvider>
       <div className="min-h-screen bg-gray-50">
         <Header onCartClick={() => setIsCartOpen(true)} />
+        <Hero />
         <Store />
         <Cart isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
       </div>
